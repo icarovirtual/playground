6383d8279f873b03f1c32aa633cea9bfbea50da4
Ícaro Raupp <budicaro@gmail.com>
2015-04-04 18:24:18 -0300

    Script pra mover scripts pro PATH do sistema. Primeira versão funcional

---
+++ common-bash/script2path.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $# -eq "0" ] || [ $1 == "-h" ]; then
+  echo \
+"script2path (script to PATH) version 1.0 by iraupph
+  include scripts in the system PATH, enabling them to be used from anywhere in the system.
+  this creates a symbolic link to your script in a default location ($HOME/bin) and gives the execution permission.
+  any modifications you make in the original file are reflected in the linked file.
+  moving the original file will probably break everything.
+
+usage: script2path [-h] script
+where:
+  -h      print this help menu
+  script  location of the script that will be moved to the system PATH
+
+complete example:
+  script2path /Users/me/scripts/my_bash.sh"
+else
+  SCRIPTS_LOCATION=$HOME/bin/
+
+  # If it's non existent, create the scripts location
+  mkdir -p "$HOME/bin"
+
+  ORIGINAL=$1
+  FILE_NAME="${ORIGINAL##*/}"
+
+  sudo ln -s "${ORIGINAL}" "${SCRIPTS_LOCATION}"
+  sudo chmod +x "${FILE_NAME}"
+
+  sh $HOME/.bash_profile
+fi
